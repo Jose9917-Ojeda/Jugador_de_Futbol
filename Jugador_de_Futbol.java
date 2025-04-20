a6491d4f74b9169d2e90c974980035f0624444a3
Jose9917-Ojeda <Josemiguelojedachaparro@gmail.com>
2025-04-20 17:50:00 -0500

    Create Jugador_de_Futbol.java

---
+++ Jugador_de_Futbol.java
@@ -0,0 +1,57 @@
+public class Jugador_de_Futbol {
+       private String Nombre;
+       private int Edad;
+       private double Estatura;
+       private double Peso;
+       private String Nacionalidad;
+       private String Posicion;
+       private String Club;
+
+       public String getNombre() {
+              return Nombre;
+
+       }
+       public void setNombre(String Nombre) {
+              this.Nombre = Nombre;
+
+       }
+       public int getEdad() {
+              return Edad;
+
+       }
+       public void setEdad(int Edad) {
+              this.Edad = Edad;
+       }
+       public double getEstatura() {
+              return Estatura;
+       }
+       public void setEstatura(double Estatura) {
+              this.Estatura = Estatura;
+       }
+       public double getPeso() {
+              return Peso;
+       }
+       public void setPeso(double Peso) {
+              this.Peso = Peso;
+
+       }
+       public String getNacionalidad() {
+              return Nacionalidad;
+       }
+       public void setNacionalidad(String Nacionalidad) {
+              this.Nacionalidad = Nacionalidad;
+       }
+       public String getPosicion() {
+              return Posicion;
+       }
+
+       public void setPosicion(String Posicion) {
+              this.Posicion = Posicion;
+       }
+       public String getClub() {
+              return Club;
+       }
+       public void setClub(String Club) {
+              this.Club = Club;
+       }
+}
